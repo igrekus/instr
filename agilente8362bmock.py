f0492b0c21871f7bdbf8a6b6327d2b366a11dad9
kef <upload234@mail.ru>
2018-11-19 18:25:19 +0300

    Add mock data

--- agilente8362bmock.py
+++ agilente8362bmock.py
@@ -6,12 +6,22 @@ class AgilentE8362BMock(AgilentE8362B):
     def __init__(self, idn: str, inst=None):
         super().__init__(idn, inst)
 
+        self._measure_num = 0
+
     def send(self, command):
         print(f'{self._name} send: {command}')
         return 'success'
 
     def query(self, question):
         answer = '-5'
+        if question == 'INITiate1:CONTinuous ON;*OPC?':
+            self._measure_num = 0
+
+        if question == 'CALCulate1:DATA? FDATA':
+            vals = [-5.5, -5.7, -6.0, -6.4, -6.9, -7.5, -8.0, -8.7, -9.6, -10.5]
+            answer = ','.join([str(v - self._measure_num / 2) for v in vals])
+            self._measure_num += 1
+
         print(f'{self._name} query: {question}, answer: {answer}')
         return answer
 
