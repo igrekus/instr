9dba71d6441b0296bdc671d327d07180153ad3d4
igrekus <upload234@mail.ru>
2018-12-12 18:21:09 +0300

    ref: change messages to English

--- obzor304.py
+++ obzor304.py
@@ -13,6 +13,7 @@ class Obzor304:
         self._inst = self._rm.open_resource(address)
         self._idn = self.query('*IDN?')
         self._folder = ''
+        self._delay = 0.7
 
     def __str__(self):
         return f'{self._idn} at {self._address.strip("TCPIP::").strip("::INSTR")}'
@@ -31,17 +32,17 @@ class Obzor304:
         self.set_trigger_source('INT')
 
     def set_touchstone_file_type(self, ftype: str):
-        print('OBZOR-304: устанавливаем тип выходного файла', self.send(f'MMEM:STOR:SNP:TYPE:{ftype}'), f'| file_type={ftype}')
+        print('OBZOR-304: set out file type', self.send(f'MMEM:STOR:SNP:TYPE:{ftype}'), f'| file_type={ftype}')
 
     def set_active_window(self, num: int):
-        print('OBZOR-304: устанавливаем активное окно:', self.send(f'DISP:WIND{num}:ACT'))
+        print('OBZOR-304: set active window:', self.send(f'DISP:WIND{num}:ACT'))
 
     def set_trigger_source(self, source: str):
-        print('OBZOR-304: устанавливаем источник триггер-сигнала:', self.send(f'TRIG:SOUR {source}'))
+        print('OBZOR-304: set trigger source:', self.send(f'TRIG:SOUR {source}'))
 
     def make_data_dir(self):
         self._folder = r'C:\!meas\LPF_' + datetime.date.today().isoformat()
-        print('OBZOR-304: создаём папку для сохранения результатов измерений:',
+        print('OBZOR-304: create out folder:',
               self.send(f'MMEM:MDIR "{self._folder}"'), f'| dir={self._folder}')
 
     def reset_instrument(self):
@@ -49,7 +50,7 @@ class Obzor304:
 
     def measure(self, n: int):
         meas_file_name = self._folder + f'\\lpf_{str(n).zfill(3)}.s2p'
-        print('OBZOR-304: запускаем измерение:', self.send('INIT1; *OPC?'))
+        print('OBZOR-304: trigger measure:', self.send('INIT1; *OPC?'))
 
         # FIXME: detector doesn't work
         # print('OBZOR-304: запускаем измерение:', self._inst.write('TRIGger:SEQuence:SINGle'))
@@ -58,15 +59,15 @@ class Obzor304:
         # while self._inst.query('*OPC?') != '1':
         #     print('waiting')
 
-        sleep(0.7)
+        sleep(self._delay)
         freqs = self.query('SENS:FREQ:DATA?')
         amps = self.query('CALC1:DATA:FDAT?')
         # print('freqs:', freqs.count(',') + 1, freqs)
         # print('amp:', amps.count(',') + 1, amps)
-        print('OBZOR-304: сохраняем результат измерений: ',
+        print('OBZOR-304: save measure:',
               self.send(f'MMEM:STOR:SNP "{meas_file_name}"'), f'\nФайл: {meas_file_name}')
         return freqs, amps
 
     def finish(self):
-        print('OBZOR-304: выключаем непрерывную подачу триггер-сигнала:', self.send('INITiate1:CONTinuous ON'))
+        print('OBZOR-304: continuous trigger off:', self.send('INITiate1:CONTinuous ON'))
         self.set_trigger_source('INT')
