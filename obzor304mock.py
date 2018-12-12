1db291030391b7213d7d49cedb68203c0db13a20
igrekus <upload234@mail.ru>
2018-12-12 18:21:30 +0300

    fix: rewrite mock

--- obzor304mock.py
+++ obzor304mock.py
@@ -1,4 +1,5 @@
 import datetime
+import random
 
 from instr.obzor304 import Obzor304
 
@@ -11,27 +12,17 @@ class Obzor304Mock(Obzor304):
         self._address = address
         self._idn = 'OBZOR304 mock'
         self._folder = ""
+        self._delay = 0
 
     def send(self, command):
         print('Obzor304 mock:', command)
         return 'success'
 
-    def measure(self, n: int):
-        meas_file_name = self._folder + f'\\lpf_{str(n).zfill(3)}.s2p'
-        print('\nOBZOR-304: запускаем измерение:', self.send('INIT1; *OPC?'))
-
-        freqs = self.query('SENS:FREQ:DATA?')
-        amps = self.query('CALC1:DATA:FDAT?')
-
-        freqs = '300,4000,50000,600000,7000000'
-        amps = '10,0,5,0,0,0,-10,0,-40,0'
-        print('freqs:', freqs.count(',') + 1, freqs)
-        print('amp:', amps.count(',') + 1, amps)
-
-        print('OBZOR-304: сохраняем результат измерений: ',
-              self.send(f'MMEM:STOR:SNP "{meas_file_name}"'), f'\nФайл:{meas_file_name}')
-        return freqs, amps
-
     def query(self, question):
         print('Obzor304 mock', question)
+        if question == 'SENS:FREQ:DATA?':
+            return '300,4000,50000,600000,7000000'
+        elif question == 'CALC1:DATA:FDAT?':
+            amps = list(map(lambda x: x - random.randint(1, 2), [2, 0, 1, 0, 1, 0, -3, 0, -6, 0]))
+            return ','.join(map(str, amps))
         return 'success'
