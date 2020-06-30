dabba5b65335d26846cbceb5de2fa2b1387b0528
igrekus <upload234@mail.ru>
2020-06-30 15:44:37 +0300

    FEAT: add n1914a power meter conrol class

---
+++ agilentn1914amock.py
@@ -0,0 +1,12 @@
+class AgilentN1914AMock:
+
+    def __init__(self):
+        pass
+
+    def write(self, command):
+        return 'success'
+
+    def query(self, question):
+        answer = '-2'
+        return answer
+
